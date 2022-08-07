ee79038aefe1f14970bbfcb8099667c09fe37f05
kimkh05 <nestjs01@naver.com>
2022-08-07 16:05:33 +0900

    ADD :: Header 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,6 @@
-export { default as MMMainLogo } from './image/mm_main_logo.svg';
-export { default as GithubLogo } from './image/github_logo.svg';
+export { default as MMMainLogo } from "./image/mm_main_logo.svg";
+export { default as GithubLogo } from "./image/github_logo.svg";
+export { default as ProfileLogo } from "./image/profile.svg";
+export { default as MentoMatch } from "./image/mento_match.svg";
+export { default as FindProject } from "./image/find_project.svg";
+export { default as Messenger } from "./image/messenger.svg";
