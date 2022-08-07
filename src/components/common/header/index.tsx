09cb3e76890b659be5eeab34c52a978d59ba4d5a
kimkh05 <nestjs01@naver.com>
2022-08-07 18:55:09 +0900

    MODIFY :: Header 퍼블리싱

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+import * as S from "./styles";
+import {
+  ProfileLogo,
+  MentoMatch,
+  Messenger,
+  FindProject,
+  MMMainLogo,
+} from "../../../assets/index";
+
 const Header = () => {
   return (
     <>
-      <span>Test Header</span>
+      <S.HeaderWrapper>
+        <S.MenuWrapper>
+          <Link className="profile" to="/profile">
+            <img src={ProfileLogo} alt="Profile Logo" />
+          </Link>
+          <Link className="menu" to="/mento">
+            <img src={MentoMatch} alt="Mento Match Logo" />
+          </Link>
+          <Link className="menu" to="/message">
+            <img src={Messenger} alt="Messenger" />
+          </Link>
+          <Link className="menu" to="/findProject">
+            <img src={FindProject} alt="Find Project" />
+          </Link>
+        </S.MenuWrapper>
+        <img className="mainLogo" src={MMMainLogo} alt="MM Main Logo" />
+      </S.HeaderWrapper>
     </>
   );
 };
--- src/components/common/header/styles.ts
+++ src/components/common/header/styles.ts
@@ -1,1 +1,39 @@
-export {};
+import styled from "@emotion/styled";
+
+export const HeaderWrapper = styled.div`
+  width: 9.6%;
+  height: 100vh;
+  background-color: #343434;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .mainLogo {
+    width: 50%;
+    height: 50%;
+  }
+`;
+
+export const MenuWrapper = styled.div`
+  width: 100%;
+  height: 70vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  .profile {
+    width: 25%;
+    height: 25%;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .menu {
+    width: 15%;
+    height: 15%;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
