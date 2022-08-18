9e4f0d85006fc0efb0b338d7f1b57a503970bf4c
kimkh05 <nestjs01@naver.com>
2022-08-19 00:37:56 +0900

    ADD :: 더미데이터 추가

---
+++ src/constance/filterList/index.ts
@@ -0,0 +1,14 @@
+export const ProgrammingDevelop: string[] = [
+  "C",
+  "C++",
+  "Java",
+  "JavaScript",
+  "Python",
+  "C#",
+];
+
+export const WebDevelop: string[] = ["Frontend", "Backend", "Design"];
+
+export const AppDevelop: string[] = ["안드로이드", "IOS"];
+
+export const Field: string[] = ["프로그래밍 언어", "웹", "앱"];
