5a05cca72fd85b84a88fba59685d5482e63ea71d
kimkh05 <nestjs01@naver.com>
2022-08-19 00:37:29 +0900

    MODIFY :: UserRouter 추가

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Match from "../components/match/index";
 import Test from "../components/test/index";
 
 const UserRouter = () => {
   return (
     <Routes>
       <Route path="/test" element={<Test />} />
+      <Route path="/mento" element={<Match />} />
     </Routes>
   );
 };
