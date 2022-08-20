5d585ec1e9e686a5a77afca9de53f740631a5318
kimkh05 <nestjs01@naver.com>
2022-08-20 10:53:01 +0900

    MODIFY :: 수정

--- src/components/match/list/index.tsx
+++ src/components/match/list/index.tsx
@@ -9,13 +9,14 @@ import {
 
 const FilterList = () => {
   const [click, setClick] = useState<boolean>(false);
-
   return (
     <>
       <S.MentoTitle>멘토 목록</S.MentoTitle>
       <S.FieldWrapper>
         {Field.map((value: string, index: number) => (
-          <span key={index}>{value}</span>
+          <div key={index} onClick={() => setClick(true)}>
+            <span>{value}</span>
+          </div>
         ))}
       </S.FieldWrapper>
     </>
