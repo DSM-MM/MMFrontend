c1fe0a3e959c8150e817c427a43c5f349003418c
kimkh05 <nestjs01@naver.com>
2022-08-29 09:15:19 +0900

    MODIFY :: 필요없는 코드 삭제

--- src/components/match/list/index.tsx
+++ src/components/match/list/index.tsx
@@ -6,7 +6,6 @@ import {
   AppDevelop,
   WebDevelop,
   ProgrammingDevelop,
-  Field,
 } from "../../../constance/filterList";
 
 function List() {
@@ -15,35 +14,16 @@ function List() {
     <>
       <S.MentoTitle>멘토 목록</S.MentoTitle>
       <S.FieldWrapper>
-        {Field.map((value: string, index: number) => (
-          <S.TopListBackground
-            key={index}
-            width={index === 0 ? 10 : 3}
-            onClick={() => (click[index] = !click[index])}
-          >
-            <span>{value}</span>
-          </S.TopListBackground>
-        ))}
+        <S.TopListBackground width={10}>
+          <span>프로그래밍 언어</span>
+        </S.TopListBackground>
+        <S.TopListBackground width={3}>
+          <span>웹</span>
+        </S.TopListBackground>
+        <S.TopListBackground width={3}>
+          <span>앱</span>
+        </S.TopListBackground>
       </S.FieldWrapper>
-      {click[0] ? (
-        <>
-          {ProgrammingDevelop.map((value: string, index: number) => (
-            <div key={index}>{value}</div>
-          ))}
-        </>
-      ) : click[1] ? (
-        <>
-          {WebDevelop.map((value, index) => (
-            <div key={index}>{value}</div>
-          ))}
-        </>
-      ) : click[2] ? (
-        <>
-          {AppDevelop.map((value, index) => (
-            <div key={index}>dfdfdf</div>
-          ))}
-        </>
-      ) : null}
       <CardListWrapper>
         {CardInformation.map((v, i) => (
           <Card
@@ -62,6 +42,8 @@ function List() {
 const CardListWrapper = styled.div`
   width: 60%;
   display: flex;
+  justify-content: center;
   flex-wrap: wrap;
+  
 `;
 export default List;
