4f62b1b140314f17c3ebe9aa67aa9ad2811810ba
kimkh05 <nestjs01@naver.com>
2022-08-29 09:15:00 +0900

    MODIFY :: 더미데이터 삭제

--- src/constance/filterList/index.ts
+++ src/constance/filterList/index.ts
@@ -10,5 +10,3 @@ export const ProgrammingDevelop: string[] = [
 export const WebDevelop: string[] = ["Frontend", "Backend", "Design"];
 
 export const AppDevelop: string[] = ["안드로이드", "IOS"];
-
-export const Field: string[] = ["프로그래밍 언어", "웹", "앱"];
