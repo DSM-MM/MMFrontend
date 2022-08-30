00f38e6bc89e8916ecf5431704c9211ab4283489
kimkh05 <nestjs01@naver.com>
2022-08-30 23:48:32 +0900

    MODIFY :: 불필요 코드 제거 & 코드 정리

--- src/components/match/list/index.tsx
+++ src/components/match/list/index.tsx
@@ -1,32 +1,57 @@
+import { useEffect, useState } from "react";
 import * as S from "./styles";
+import CardList from "../cardlist";
 import {
   AppDevelop,
   WebDevelop,
   ProgrammingDevelop,
+  DevelopList,
 } from "../../../constance/filterList";
-import CardList from "../cardlist";
+import NumberList from "./number";
 
-function List() {
-  const click: boolean[] = new Array(3).fill(false);
+interface PropsType {
+  id?: number;
+  text: string;
+  name: string;
+}
+
+const List = () => {
+  const [content, setContent] = useState();
+  const selectComponent = {
+    first: <NumberList arr={ProgrammingDevelop} />,
+    second: <NumberList arr={WebDevelop} />,
+    third: <NumberList arr={AppDevelop} />,
+  };
+  const [isSelected, setSelected] = useState<boolean[]>(
+    new Array(3).fill(false)
+  );
+
+  const handleClickButton = (e: any) => {
+    const { name } = e.target;
+    setContent(name);
+  };
+
   return (
     <>
       <div style={{ width: "70%", paddingTop: "5vh" }}>
         <S.MentoTitle>멘토 목록</S.MentoTitle>
       </div>
       <S.FieldWrapper>
-        <S.TopListBackground width={10}>
-          <span>프로그래밍 언어</span>
-        </S.TopListBackground>
-        <S.TopListBackground width={3}>
-          <span>웹</span>
-        </S.TopListBackground>
-        <S.TopListBackground width={3}>
-          <span>앱</span>
-        </S.TopListBackground>
+        {DevelopList.map((data: PropsType) => (
+          <S.TopListBackground
+            onClick={handleClickButton}
+            key={data.id}
+            name={data.name}
+            width={data.id === 1 ? 10 : 3}
+          >
+            <span>{data.text}</span>
+          </S.TopListBackground>
+        ))}
       </S.FieldWrapper>
+      {content && <div>{selectComponent[content]}</div>}
       <CardList />
     </>
   );
-}
+};
 
 export default List;
