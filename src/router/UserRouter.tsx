91020d7dda987d707768e840c9c7e8e8fe4a5b5d
kimkh05 <nestjs01@naver.com>
2022-09-05 19:18:17 +0900

    MODIFY :: Error 수정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,11 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Match from "../components/match/index";
-import Test from "../components/test/index";
 import Introduce from "../components/introduce/index";
-=======
 import Test from "../components/test";
->>>>>>> main
 
 const UserRouter = () => {
   return (
