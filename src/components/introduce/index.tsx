35b59d64c06ae7e0f28ef50bddd976559fd70813
kimkh05 <nestjs01@naver.com>
2022-09-06 19:24:13 +0900

    MODIFY :: 퍼블리싱 완료

--- src/components/introduce/index.tsx
+++ src/components/introduce/index.tsx
@@ -1,17 +1,29 @@
 import { useParams } from "react-router";
 import Header from "../common/header";
 import * as S from "./styles";
+import MentoCard from "./card";
+import { Link } from "react-router-dom";
 
 const Introduce: React.FC = () => {
   const { job, name } = useParams();
-  console.log(job, name);
   return (
     <>
       <div style={{ display: "flex" }}>
         <Header />
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <S.IntroduceText>멘토 소개</S.IntroduceText>
-        </div>
+        <S.IntroduceWrapper>
+          <div style={{ width: "100%" }}>
+            <div style={{ marginBottom: "20px" }}>
+              <S.IntroduceText>멘토 소개</S.IntroduceText>
+            </div>
+            <MentoCard job={job} name={name} />
+            <S.ButtonWrapper>
+              <S.LinkButton>질문하러 가기</S.LinkButton>
+              <Link to="/mento">
+                <S.LinkButton>다른 멘토 찾기</S.LinkButton>
+              </Link>
+            </S.ButtonWrapper>
+          </div>
+        </S.IntroduceWrapper>
       </div>
     </>
   );
--- src/components/introduce/styles.ts
+++ src/components/introduce/styles.ts
@@ -1,8 +1,35 @@
 import styled from "@emotion/styled";
+import theme from "../../styles/theme";
 
 export const IntroduceText = styled.span`
   font-weight: 700;
   font-size: 32px;
   line-height: 44px;
   text-align: center;
+`;
+
+export const IntroduceWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  place-content: center;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 800px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const LinkButton = styled.button`
+  width: 200px;
+  height: 40px;
+  background-color: ${theme.color.maincolor};
+  border-radius: 10px;
+  color: ${theme.color.white000};
+  margin-top: 80px;
+  &:hover {
+    background-color: ${theme.color.white000};
+    color: ${theme.color.black000};
+    border: 1px solid ${theme.color.black000};
+  }
 `;
