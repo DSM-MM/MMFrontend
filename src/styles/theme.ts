484b796d46aec9067699c6db67b7017974ecf4df
kimkh05 <nestjs01@naver.com>
2022-09-06 19:23:26 +0900

    MODIFY :: color 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,11 @@
 const color = {
   maincolor: "#343434",
   white000: "#ffffff",
+  white001: "#fcfcfc",
   gray000: "#999999",
   gray001: "#D9D9D9",
   gray002: "#aaaaaa",
+  black000: "#000000",
 };
 
 const font = {
