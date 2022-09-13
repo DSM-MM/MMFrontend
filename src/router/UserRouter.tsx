19a79aa484817b5f84a7e4ae6ca49c2991827fd4
kimkh05 <nestjs01@naver.com>
2022-09-13 17:32:01 +0900

    MODIFY :: 라우터 설정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import FindProject from "../components/find";
 import Test from "../components/test/";
 
 const UserRouter = () => {
   return (
     <Routes>
       <Route path="/test" element={<Test />} />
+      <Route path="/find" element={<FindProject />} />
     </Routes>
   );
 };
