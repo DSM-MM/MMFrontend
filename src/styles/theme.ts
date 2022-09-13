0d487ce42d2f3e884267dd38a567c16ec6d5200e
kimkh05 <nestjs01@naver.com>
2022-09-13 17:32:13 +0900

    ADD :: Theme 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+const color = {
+  maincolor: "#343434",
+  white000: "#ffffff",
+  white001: "#fcfcfc",
+  gray000: "#999999",
+  gray001: "#D9D9D9",
+  gray002: "#aaaaaa",
+  gray003: "#919191",
+  black000: "#000000",
+};
+
+const font = {
+  mainfont: "Noto Sans",
+};
+
+const theme = {
+  color: color,
+  font: font,
+};
+
+export default theme;
