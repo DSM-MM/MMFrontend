45c63afbf0af53cb83635d3dfd73201bceb21991
kimkh05 <nestjs01@naver.com>
2022-09-15 20:08:07 +0900

    ADD :: 404 page 완성

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -3,16 +3,18 @@ import Match from "../components/match/index";
 import Introduce from "../components/introduce/index";
 import Main from "../components/main/index";
 import Test from "../components/test";
+import NotFound from "../pages/404";
 
 const UserRouter = () => {
   return (
     <Routes>
-      <Route path="/test" element={<Test />} />
+      <Route path="/" element={<Test />} />
       <Route path="/mento" element={<Match />} />
       <Route path="/mento/:job/:name" element={<Introduce />} />
       <Route path="/main" element={<Main />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
-}; 
+};
 
 export default UserRouter;
