cd34c0ccb1d4b7f5208ad2ff766e1f58735d9d2e
kimkh05 <nestjs01@naver.com>
2022-09-20 17:03:54 +0900

    MODIFY :: 오류 수정

--- src/router/UserRouter.tsx
+++ src/router/UserRouter.tsx
@@ -1,16 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import FindProject from "../components/find";
-<<<<<<< HEAD
 import Match from "../components/match/index";
 import Introduce from "../components/introduce/index";
 import Main from "../components/main/index";
 import Test from "../components/test";
 import NotFound from "../pages/404";
-=======
 import CreateProject from "../components/create";
-import Test from "../components/test/";
 import ViewProject from "../components/view";
->>>>>>> findProject
 
 const UserRouter = () => {
   return (
@@ -18,15 +14,12 @@ const UserRouter = () => {
       <Route path="/" element={<Test />} />
       <Route path="/test" element={<Test />} />
       <Route path="/find" element={<FindProject />} />
-<<<<<<< HEAD
       <Route path="/mento" element={<Match />} />
       <Route path="/mento/:job/:name" element={<Introduce />} />
       <Route path="/main" element={<Main />} />
       <Route path="/*" element={<NotFound />} />
-=======
       <Route path="/find/create" element={<CreateProject />} />
       <Route path="/find/:id" element={<ViewProject />} />
->>>>>>> findProject
     </Routes>
   );
 };
