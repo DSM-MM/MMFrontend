3a07b2946a6d7a218ee8b6e3a5d2b481a89990c8
kimkh05 <nestjs01@naver.com>
2022-10-18 20:12:10 +0900

    MODIFY :: modify dummy data

--- src/constance/signup/index.ts
+++ src/constance/signup/index.ts
@@ -1,8 +1,13 @@
-export const Job: string[] = [
-  "Frontend",
-  "Backend",
-  "Android",
-  "IOS",
-  "Web Design",
-  "App Design",
+interface DummyType {
+  id: number;
+  job: string;
+}
+
+export const Job: DummyType[] = [
+  { id: 1, job: "Frontend" },
+  { id: 2, job: "Backend" },
+  { id: 3, job: "Android" },
+  { id: 4, job: "IOS" },
+  { id: 5, job: "Web Design" },
+  { id: 6, job: "App Design" },
 ];
