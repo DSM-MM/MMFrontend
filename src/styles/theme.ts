35b050266f180dbc9e5eb25a19a26ecc1487f8f4
kimkh05 <nestjs01@naver.com>
2022-10-18 11:51:57 +0900

    MODIFY :: adding theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,11 +2,13 @@ const color = {
   maincolor: "#343434",
   white000: "#ffffff",
   white001: "#fcfcfc",
+  white002: "#FDFDFD",
   gray000: "#999999",
   gray001: "#D9D9D9",
   gray002: "#aaaaaa",
   gray003: "#919191",
   gray004: "#666666",
+  gray005: "#444444",
   black000: "#000000",
 };
 
