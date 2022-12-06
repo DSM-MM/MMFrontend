d1c9c78319aebf199f7b67aa5588f0dfabd09b74
kimkh05 <nestjs01@naver.com>
2022-12-06 19:24:20 +0900

    refactor : code

--- src/components/main/mento/index.tsx
+++ src/components/main/mento/index.tsx
@@ -8,21 +8,17 @@ const Mento = () => {
       <S.MentoWrapper>
         <S.Title>평점이 높은 멘토 TOP 3</S.Title>
         <S.CardWrapper>
-          {List.map((value, index) => {
-            return (
-              <>
-                <S.Card key={index}>
-                  <S.CardText>{value.job}</S.CardText>
-                  <S.ProfileCircle>
-                    <img src={ProfileLogo} alt="Profile Img" />
-                  </S.ProfileCircle>
-                  <S.CardText>{value.name}</S.CardText>
-                  <S.CardText>"{value.introduce}"</S.CardText>
-                  <S.GPA>평점 : {value.grade}</S.GPA>
-                </S.Card>
-              </>
-            );
-          })}
+          {List.map((value, index) => (
+            <S.Card key={index}>
+              <S.CardText>{value.job}</S.CardText>
+              <S.ProfileCircle>
+                <img src={ProfileLogo} alt="Profile Img" />
+              </S.ProfileCircle>
+              <S.CardText>{value.name}</S.CardText>
+              <S.CardText>"{value.introduce}"</S.CardText>
+              <S.GPA>평점 : {value.grade}</S.GPA>
+            </S.Card>
+          ))}
         </S.CardWrapper>
       </S.MentoWrapper>
     </>
