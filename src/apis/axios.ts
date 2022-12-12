9c8dc6210621f130f0bf8c7c9e42bddb1a9c6bb5
kimkh05 <nestjs01@naver.com>
2022-12-12 20:24:48 +0900

    feat: instance 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,7 +1,19 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = localStorage.getItem("access_token");
+    accessToken &&
+      (config.headers = { Authorization: `Bearer ${accessToken}` });
+    return config;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
