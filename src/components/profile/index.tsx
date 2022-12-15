0dcf0fb20397f8cfd0b422e6f4414096b0ab4120
kimkh05 <nestjs01@naver.com>
2022-12-15 20:22:07 +0900

    feat: init edit function

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,28 +1,22 @@
 import Header from "../common/header";
 import * as S from "./styles";
-import { ProfileImage } from "../../assets";
+import styled from "@emotion/styled";
+import { ProfileImage, GithubSvg } from "../../assets";
 import { useEffect, useState } from "react";
 import { getProfile, ProfileType, ProjectType } from "../../apis/profile";
-
-const Pencil = () => {
-  return (
-    <S.Pencil
-      width="15"
-      height="15"
-      viewBox="0 0 15 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M14.5869 4.16105L13.2362 5.51169C13.0985 5.64939 12.8759 5.64939 12.7382 5.51169L9.48607 2.25961C9.34837 2.12191 9.34837 1.89925 9.48607 1.76154L10.8367 0.410905C11.3846 -0.136968 12.2753 -0.136968 12.8261 0.410905L14.5869 2.17172C15.1377 2.71959 15.1377 3.61025 14.5869 4.16105ZM8.32586 2.92175L0.632148 10.6154L0.0110251 14.1751C-0.0739398 14.6556 0.345025 15.0716 0.825516 14.9896L4.38525 14.3656L12.079 6.6719C12.2167 6.5342 12.2167 6.31153 12.079 6.17383L8.82686 2.92175C8.68623 2.78405 8.46357 2.78405 8.32586 2.92175ZM3.63522 9.95621C3.47408 9.79507 3.47408 9.53724 3.63522 9.3761L8.14714 4.86421C8.30828 4.70307 8.56611 4.70307 8.72725 4.86421C8.88839 5.02535 8.88839 5.28317 8.72725 5.44431L4.21532 9.95621C4.05418 10.1173 3.79636 10.1173 3.63522 9.95621ZM2.57755 12.4202H3.98387V13.4837L2.09413 13.8148L1.18295 12.9036L1.51402 11.0139H2.57755V12.4202Z"
-        fill="white"
-      />
-    </S.Pencil>
-  );
-};
+import { customToast } from "../../util/toast";
+import { setMy, MyType } from "../../apis/setMy";
+import Pencil from "./pencil";
 
 const Profile = () => {
   const [userState, setUserState] = useState<ProfileType>();
+  const [changeState, setChangeState] = useState<MyType>({
+    email: "",
+    introduction: "",
+    jobGroup: "",
+    language: "",
+    githubLink: "",
+  });
   const [update, setUpdate] = useState<boolean>(false);
   useEffect(() => {
     getProfile()
@@ -30,7 +24,7 @@ const Profile = () => {
         if (res) {
           setUserState(res.data);
           console.log(res);
-        };
+        }
       })
       .catch((err) => console.error(err));
   }, []);
@@ -38,6 +32,29 @@ const Profile = () => {
     window.localStorage.removeItem("access_token");
     window.localStorage.removeItem("refresh_token");
     window.location.href = "/";
+  };
+  const onChange = (value: string, dir: string) => {
+    setChangeState({
+      ...changeState,
+      [dir]: value,
+    });
+  };
+  const gotoGithubLinkOnClick = () => {
+    if (userState?.githubLink) {
+      const githubId: string = userState.githubLink;
+      window.location.href = `https://github.com/${githubId}`;
+    } else {
+      customToast("깃허브를 등록하지 않았습니다.", "error");
+    }
+  };
+  const edit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!update) {
+      setUpdate(true);
+      return;
+    }
+    setMy(changeState)
+      .then(() => window.location.reload())
+      .catch((error) => console.error(error));
   };
   return (
     <S.Body>
@@ -56,38 +73,114 @@ const Profile = () => {
             </S._ProfileWrapper>
             <S._Wrapper>
               <S.Logout onClick={logoutOnClick}>로그아웃</S.Logout>
-              <S.ProfileEditButton onClick={() => setUpdate(true)}>
-                <Pencil />
-                프로필 편집
+              <S.ProfileEditButton onClick={edit}>
+                {update ? "편집 완료" : "프로필 편집"}
               </S.ProfileEditButton>
             </S._Wrapper>
           </S.ProfilePictureDiv>
-          <S.Content>
-            <S.Name>{userState?.nickName}</S.Name>
-            <S.Introduce>{userState?.introduction}</S.Introduce>
-            <S.InfoDiv>
-              <S.Info>
-                <S.InfoTitle>Email</S.InfoTitle>
-                <S.InfoContent>{userState?.email}</S.InfoContent>
-              </S.Info>
-              <S.Info>
-                <S.InfoTitle>비밀번호</S.InfoTitle>
-                <S.InfoContent></S.InfoContent>
-              </S.Info>
-              <S.Info>
-                <S.InfoTitle>평점</S.InfoTitle>
-                <S.InfoContent>{userState?.provider}</S.InfoContent>
-              </S.Info>
-              <S.Info>
-                <S.InfoTitle>직군</S.InfoTitle>
-                <S.InfoContent>{userState?.jobGroup} Developer</S.InfoContent>
-              </S.Info>
-              <S.Info>
-                <S.InfoTitle>사용언어</S.InfoTitle>
-                <span>{userState?.language}</span>
-              </S.Info>
-            </S.InfoDiv>
-          </S.Content>
+          {!update ? (
+            <S.Content>
+              <_NameWrapper>
+                <S.Name>{userState?.nickName}</S.Name>
+                <_GithubImage
+                  src={GithubSvg}
+                  onClick={gotoGithubLinkOnClick}
+                  alt="go to github"
+                />
+              </_NameWrapper>
+              <S.Introduce>{userState?.introduction}</S.Introduce>
+              <S.InfoDiv>
+                <S.Info>
+                  <S.InfoTitle>Email</S.InfoTitle>
+                  <S.InfoContent>{userState?.email}</S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>비밀번호</S.InfoTitle>
+                  <S.InfoContent></S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>평점</S.InfoTitle>
+                  <S.InfoContent>{userState?.provider}</S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>직군</S.InfoTitle>
+                  <S.InfoContent>{userState?.jobGroup} Developer</S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>사용언어</S.InfoTitle>
+                  <span>{userState?.language}</span>
+                </S.Info>
+              </S.InfoDiv>
+            </S.Content>
+          ) : (
+            <S.Content>
+              <S.Name>{userState?.nickName}</S.Name>
+              <S.Introduce>
+                <_IntroductionWrapper>
+                  <_Input
+                    value={changeState.introduction}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      onChange(e.target.value, "introduction")
+                    }
+                    placeholder="자기소개를 입력하세요."
+                  />
+                </_IntroductionWrapper>
+              </S.Introduce>
+              <S.InfoDiv>
+                <S.Info>
+                  <S.InfoTitle>Email</S.InfoTitle>
+                  <S.InfoContent>
+                    <_Input
+                      value={changeState.email}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        onChange(e.target.value, "email")
+                      }
+                      placeholder="이메일을 입력하세요."
+                    />
+                  </S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>비밀번호</S.InfoTitle>
+                  <S.InfoContent>
+                    <Pencil />
+                  </S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>직군</S.InfoTitle>
+                  <S.InfoContent>
+                    <_Input
+                      value={changeState.jobGroup}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        onChange(e.target.value, "jobGroup")
+                      }
+                      placeholder="직군을 입력하세요."
+                    />{" "}
+                    Developer
+                  </S.InfoContent>
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>사용언어</S.InfoTitle>
+                  <_Input
+                    value={changeState.language}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      onChange(e.target.value, "language")
+                    }
+                    placeholder="사용언어를 입력하세요."
+                  />
+                </S.Info>
+                <S.Info>
+                  <S.InfoTitle>Github ID</S.InfoTitle>
+                  <_Input
+                    value={changeState.githubLink}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      onChange(e.target.value, "githubLink")
+                    }
+                    placeholder="Github 프롤필을 입력하세요."
+                  />
+                </S.Info>
+              </S.InfoDiv>
+            </S.Content>
+          )}
           <S.ProjectDiv>
             <S.MyProjects>내 프로젝트</S.MyProjects>
             <S.ProjectList>
@@ -110,5 +203,33 @@ const Profile = () => {
     </S.Body>
   );
 };
+
+const _Input = styled.input`
+  background-color: transparent;
+`;
+
+const _IntroductionWrapper = styled.div`
+  width: 1200px;
+  height: 90px;
+  background-color: #f8f8f8;
+  border: 1px solid #d9d9d9;
+  border-radius: 10px;
+  padding: 8px 12px;
+  input {
+    width: 100%;
+  }
+`;
+
+const _NameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 13px;
+`;
+
+const _GithubImage = styled.img`
+  width: 15px;
+  height: 15px;
+  cursor: pointer;
+`;
 
 export default Profile;
