b502aa516c6bf20be07b9131a05de93b1bda2858
kimkh05 <nestjs01@naver.com>
2022-12-15 15:35:52 +0900

    fix: make mento button

--- src/components/match/list/index.tsx
+++ src/components/match/list/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import * as S from "./styles";
+import styled from "@emotion/styled";
 import CardList from "../cardlist";
 import {
   DevelopList,
@@ -53,6 +54,16 @@ const List = () => {
       <S.FieldWrapper>
         {DevelopList.map((data) => getButton(data))}
       </S.FieldWrapper>
+      <NewAddMento>
+        <S.TopListBackground
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
+            (window.location.href = "/mento/create")
+          }
+          width={10}
+        >
+          멘토 생성
+        </S.TopListBackground>
+      </NewAddMento>
       <S.ListWrapper>
         {content && (
           <S.SelectedContentWrapper>
@@ -64,5 +75,9 @@ const List = () => {
     </>
   );
 };
+
+const NewAddMento = styled.div`
+  width: 60%;
+`;
 
 export default List;
