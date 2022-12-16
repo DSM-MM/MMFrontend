db1b2a13852654c823fd56d58fd02392ca3aa4db
kimkh05 <nestjs01@naver.com>
2022-12-16 16:20:15 +0900

    feat: init top3 types

--- src/apis/top3.ts
+++ src/apis/top3.ts
@@ -1,6 +1,17 @@
 import instance from "./axios";
 
-export const getTopThree = async() => {
-    const response = await instance.get('/mentor/top3');
-    return response;
+export interface TopThreeType {
+  id: number;
+  name: string;
+  major: string;
+  email: string;
+  introduction: string;
+  language: string;
+  rating: number;
+  jobGroup: string;
 }
+
+export const getTopThree = async () => {
+  const response = await instance.get<TopThreeType[]>("/mentor/top3");
+  return response;
+};
