3c7942703e6eadfe0fdd90393ef0882fd92f5ed2
kimkh05 <nestjs01@naver.com>
2022-12-19 19:24:49 +0900

    fix: style

--- src/components/match/list/number/index.tsx
+++ src/components/match/list/number/index.tsx
@@ -16,9 +16,10 @@ const NumberList: React.FC<PropsType> = ({ list }: PropsType) => (
 
 const Btn = styled.button`
   margin-right: 30px;
-  width: 4rem;
+  width: max-content;
   height: 40px;
   border-radius: 1rem;
+  padding: 5px 20px;
   background-color: white;
   &:hover {
     background-color: #343434;
