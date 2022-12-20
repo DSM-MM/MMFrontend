f8556ca022c5b57fd67eda7f6b75d966717af336
kimkh05 <nestjs01@naver.com>
2022-12-20 14:20:25 +0900

    feat: if click move scroll

--- src/components/mainpage/index.tsx
+++ src/components/mainpage/index.tsx
@@ -11,11 +11,10 @@ import {
 import Footer from "../common/footer";
 import { useEffect, useState, useRef } from "react";
 import LoginModal from "./loginmodal";
-import useMoveScrool from "../../hooks/useMoveScroll";
+import { Link } from "react-scroll";
 
 const MainPage = () => {
   const [modal, setModal] = useState<boolean>(false);
-  const ref = useRef<HTMLDivElement>();
   useEffect(() => {
     if (modal) {
       document.body.style.overflow = "hidden";
@@ -30,13 +29,17 @@ const MainPage = () => {
       {modal && <LoginModal setModal={setModal} />}
       <S.ImgWrapper>
         <S.LeftImg src={LeftShape} alt="shpae" />
-        <S.ArrowWrapper>
+        <S.ArrowWrapper to="first" spy={true} smooth={true}>
           <S.ArrowImg src={BottomArrow} alt="to move bottom" />
         </S.ArrowWrapper>
         <S.RightImg src={RightShape} alt="shape" />
       </S.ImgWrapper>
-      <S.IntroduceWrapper>
-        <S.ArrowImg src={TopArrow} alt="to move top" />
+      <S.IntroduceWrapper id="first">
+        <S.ArrowImg
+          src={TopArrow}
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          alt="to move top"
+        />
         <S.TextWrapper>
           <S.Text>
             MM은 랜덤으로 각 분야의 전문가들을 매칭시켜줍니다. <br />
@@ -44,9 +47,11 @@ const MainPage = () => {
           </S.Text>
           <img alt="이미지1" src={Menu1} />
         </S.TextWrapper>
-        <S.ArrowImg src={BottomArrow} alt="to move bottom" />
+        <Link to="second" spy={true} smooth={true}>
+          <S.ArrowImg src={BottomArrow} alt="to move bottom" />
+        </Link>
       </S.IntroduceWrapper>
-      <S.IntroduceWrapper>
+      <S.IntroduceWrapper id="second">
         <S.ArrowImg src={TopArrow} alt="to move top" />
         <S.TextWrapper>
           <img alt="이미지1" src={Menu2} />
@@ -55,9 +60,11 @@ const MainPage = () => {
             멘토와 소통하여 함께 전공 실력을 쌓아가세요 !
           </S.Text>
         </S.TextWrapper>
-        <S.ArrowImg src={BottomArrow} alt="to move bottom" />
+        <Link to="third" spy={true} smooth={true}>
+          <S.ArrowImg src={BottomArrow} alt="to move bottom" />
+        </Link>
       </S.IntroduceWrapper>
-      <S.IntroduceWrapper>
+      <S.IntroduceWrapper id="third">
         <S.ArrowImg src={TopArrow} alt="to move top" />
         <S.TextWrapper>
           <S.Text>
