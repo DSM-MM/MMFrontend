8ad988c2009acbbe14d58854e741659770bdf718
kimkh05 <nestjs01@naver.com>
2022-12-28 18:52:48 +0900

    fix: scroll bug

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -9,20 +9,23 @@ import {
 import styled from "@emotion/styled";
 
 const Header: React.FC = () => {
+  const gotoTop = async () => {
+    await window.scrollTo(0, 0);
+  };
   return (
     <S.HeaderWrapper>
       <S.MenuWrapper>
-        <Link className="profile" to="/profile">
+        <Link className="profile" onClick={gotoTop} to="/profile">
           <img src={ProfileLogo} alt="Profile Logo" />
         </Link>
-        <_Link to="/mento">
+        <_Link onClick={gotoTop} to="/mento">
           <img src={MentoMatch} alt="Mento Match Logo" />
         </_Link>
-        <Link to="/find">
+        <Link onClick={gotoTop} to="/find">
           <img src={FindProject} alt="Find" />
         </Link>
       </S.MenuWrapper>
-      <Link className="main" to="/main">
+      <Link className="main" onClick={gotoTop} to="/main">
         <img className="mainLogo" src={MMMainLogo} alt="MM Main Logo" />
       </Link>
     </S.HeaderWrapper>
