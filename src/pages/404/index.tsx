f54ca343ad3e81d6afdc37f233cedce1b805b5a4
kimkh05 <nestjs01@naver.com>
2022-12-30 09:47:40 +0900

    fix: init nav

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,26 +1,35 @@
 import { Page404 } from "../../assets";
 import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
 
-const Image = styled.img`
+const NotFound = () => {
+  const nav = useNavigate();
+  const goBackPage = (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    nav(-1);
+  };
+  return (
+    <_Wrapper>
+      <_Image src={Page404} alt="page404" />
+      <_Text onClick={goBackPage}>뒤로 가기</_Text>
+    </_Wrapper>
+  );
+};
+
+const _Image = styled.img`
   width: 50vw;
   height: 50vh;
 `;
 
-const Wrapper = styled.div`
+const _Wrapper = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
   place-content: center;
 `;
 
-const NotFound = () => {
-  return (
-    <>
-      <Wrapper>
-        <Image src={Page404} alt="page404" />
-      </Wrapper>
-    </>
-  );
-};
+const _Text = styled.span`
+  cursor: pointer;
+`;
 
 export default NotFound;
